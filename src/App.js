902358c4ac3cb3be0eb6427481e3eaf073f63e01
Chris Matthews <chris@matthews.digital>
2019-10-21 19:55:44 +0100

    Added initial API integration

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import Header from './components/Header'
+import React, { useState, useEffect } from 'react';
+import { getCustomer } from './api';
+import Header from './components/Header';
 import './App.css';
 
-function App() {
+const App = () => {
+    const [customer, setCustomer] = useState([{}]);
+
+    useEffect(() => {
+        const getData = async () => setCustomer(await getCustomer());
+        getData();
+    }, []);
+
   return (
     <>
       <Header />
       <main className="main">
         <section className="content">
-            <h1>Welcome, &lt;name&gt;!</h1>
+            <h1>Welcome, {customer.first_name}!</h1>
 
             <section className="grid-col-2">
                 <section className="account-summary">
---
+++ src/api/index.js
@@ -0,0 +1,7 @@
+export const getCustomer = async () => {
+    const response = await fetch(`http://api.reimaginebanking.com/customers?key=${process.env.REACT_APP_NESSIE_API_KEY}`);
+    const data = await response.json();
+    return data[0];
+}
+
+// export default { getCustomer };
